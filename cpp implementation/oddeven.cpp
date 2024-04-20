df24666477e6c499e18f77df1267f000daf9ac53
Akshaya-Mandepudi <149465696+Akshaya-Mandepudi@users.noreply.github.com>
2024-04-20 16:13:52 +0530

    Update oddeven.cpp

--- cpp implementation/oddeven.cpp
+++ cpp implementation/oddeven.cpp
@@ -1,57 +1,58 @@
-// A C++ Program to implement Odd-Even / Brick Sort
-#include<bits/stdc++.h>
+#include <iostream>
+#include <fstream> // For file operations
+#include <cstdlib> // For exit()
+
 using namespace std;
- 
+
 // A function to sort the algorithm using Odd Even sort
-void oddEvenSort(int arr[], int n)
-{
+void oddEvenSort(int arr[], int n) {
     bool isSorted = false; // Initially array is unsorted
- 
-    while (!isSorted)
-    {
+
+    while (!isSorted) {
         isSorted = true;
- 
+
         // Perform Bubble sort on odd indexed element
-        for (int i=1; i<=n-2; i=i+2)
-        {
-            if (arr[i] > arr[i+1])
-             {
-                swap(arr[i], arr[i+1]);
+        for (int i = 1; i <= n - 2; i = i + 2) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
                 isSorted = false;
-              }
+            }
         }
- 
+
         // Perform Bubble sort on even indexed element
-        for (int i=0; i<=n-2; i=i+2)
-        {
-            if (arr[i] > arr[i+1])
-            {
-                swap(arr[i], arr[i+1]);
+        for (int i = 0; i <= n - 2; i = i + 2) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
                 isSorted = false;
             }
         }
     }
- 
-    return;
 }
- 
+
 // A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-   for (int i=0; i < n; i++)
-       cout << arr[i] << " ";
-   cout << "\n";
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
 }
- 
+
 // Driver program to test above functions.
-int main()
-{
-    int arr[] = {34, 2, 10, -9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<"Sorted array:";
- 
+int main() {
+    ifstream inputFile("input.txt"); // Open input file
+    if (!inputFile.is_open()) {
+        cout << "Error: Unable to open input file!" << endl;
+        exit(EXIT_FAILURE); // Exit the program with an error code
+    }
+
+    int arr[100], n = 0;
+    while (n < 100 && inputFile >> arr[n]) { // Read integers from input file
+        n++;
+    }
+    inputFile.close(); // Close input file
+
+    cout << "Sorted array: ";
     oddEvenSort(arr, n);
     printArray(arr, n);
- 
-    return (0);
+
+    return 0;
 }
