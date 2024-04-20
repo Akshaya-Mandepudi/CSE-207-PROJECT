af6d8c830d284e6862fa98e61556ff2cd9ac468a
Akshaya-Mandepudi <149465696+Akshaya-Mandepudi@users.noreply.github.com>
2024-04-20 17:09:43 +0530

    Add files via upload

--- python implementation/oe.py
+++ python implementation/oe.py
@@ -1,1 +1,31 @@
+def oddEvenSort(arr):
+    n = len(arr)
+    isSorted = 0
+    
+    while isSorted == 0:
+        isSorted = 1
+        for i in range(1, n-1, 2):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                isSorted = 0
+        for i in range(0, n-1, 2):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                isSorted = 0
 
+def printArray(arr):
+    for num in arr:
+        print(num, end=" ")
+    print()
+
+def main():
+    with open("input.txt", "r") as file:
+        numTestCases = int(file.readline())
+        for _ in range(numTestCases):
+            n = int(file.readline())
+            arr = list(map(int, file.readline().split()))
+            oddEvenSort(arr)
+            printArray(arr)
+
+if __name__ == "__main__":
+    main()
